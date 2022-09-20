228f1838db8698b389aa960cafc846fc8f23e592
lynnw5198 <113375681+lynnw5198@users.noreply.github.com>
2022-09-19 22:12:11 -0600

    Add files via upload

---
+++ LorilynWang.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 19 22:10:00 2022
+
+@author: LOOOOOOO
+"""
+
+print("L")
+print("O")
+print("R")
+print("I")
+print("L")
+print("Y")
+print("N")
+
