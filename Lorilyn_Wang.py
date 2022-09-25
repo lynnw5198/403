5f1251ef89ec27071e2e4091a82831620ccc455c
lynnw5198 <113375681+lynnw5198@users.noreply.github.com>
2022-09-25 15:42:01 -0600

    Add files via upload

---
+++ Lorilyn_Wang.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 24 21:14:38 2022
+
+@author: LOOOOOOO
+"""
+
+letter1="L"
+letter2 = "O"
+letter3 ="R"
+letter4 ="I"
+letter5="L"
+letter6="Y"
+letter7="N"
+
+print(letter1)
+print(letter2)
+print(letter3)
+print(letter4)
+print(letter5)
+print(letter6)
+print(letter7)
+
+#L
+#O
+#R
+#I
+#L
+#Y
+#N
+
+letterX="K"
+print(letterX)
+#K
+print(letter1)
+#L
+
+letterX=letter1
+print(letterX)
+#L
+
+print(letter1)
+#L
+
+letter1="Z"
+print(letterX)
+#L
+print(letter1)
+#Z
+
+#No because changing 1 to Z doesn't change it- L stays the same
+#The value doesn't depend on the variable (X doesnt equal KFelkn)
