8050af0a9d293210cace5178b9aeaea0f06abb7a
lynnw5198 <113375681+lynnw5198@users.noreply.github.com>
2022-09-25 22:34:37 -0600

    Update LorilynWang.py

--- LorilynWang.py
+++ LorilynWang.py
@@ -1,16 +1,1 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 19 22:10:00 2022
-
-@author: LOOOOOOO
-"""
-
-print("L")
-print("O")
-print("R")
-print("I")
-print("L")
-print("Y")
-print("N")
 
